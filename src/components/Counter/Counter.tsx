7ec074b6f1b1becf0d5df332c41b8021df6db307
LeonidasZoidis <lzoidis123@gmail.com>
2024-06-29 16:43:55 +0100

    added sound effects, optimised desktop layout

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,19 +1,39 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Button from '../Button/Button';
 import { RxReset } from 'react-icons/rx';
+import clickSound1 from '../../assets/click-button-140881.mp3';
+import clickSound2 from '../../assets/high-pitch-click-47137.mp3';
 
 const Counter = () => {
-    const [count, setCount] = useState<number>(0);
+    const [count, setCount] = useState<number>(() => {
+        const storedCount = localStorage.getItem('storedCount');
+        return storedCount !== null ? JSON.parse(storedCount) : 0;
+    });
+
+    const clickAudio = useRef(new Audio(clickSound1));
+    const resetAudio = useRef(new Audio(clickSound2));
 
     const handleIncrement = () => {
+        clickAudio.current.play();
         setCount((prevCount) => prevCount + 1);
     };
     const handleDecrement = () => {
+        clickAudio.current.play();
         setCount((prevCount) => (prevCount > 0 ? prevCount - 1 : 0));
     };
     const resetCount = () => {
+        resetAudio.current.play();
         setCount(0);
     };
+
+    useEffect(() => {
+        localStorage.setItem('storedCount', JSON.stringify(count));
+    }, [count]);
+
+    useEffect(() => {
+        clickAudio.current.preload = 'auto';
+        resetAudio.current.preload = 'auto';
+    }, []);
 
     return (
         <>
@@ -34,8 +54,8 @@ const Counter = () => {
                     >
                         <RxReset className="!text-6xl text-lime-600" />
                     </Button>
-                    <div className="flex gap-3 my-8 w-full h-auto">
-                        <div className="w-1/2">
+                    <div className="flex items-center justify-center gap-3 my-8 w-full h-auto">
+                        <div className="w-full sm:w-1/2 lg:w-1/4">
                             <Button
                                 disabled={count < 1 ? true : false}
                                 className="w-full h-24 font-thin !text-6xl text-yellow-400 !bg-lime-700"
@@ -44,7 +64,7 @@ const Counter = () => {
                                 -
                             </Button>
                         </div>
-                        <div className="w-1/2">
+                        <div className="w-full sm:w-1/2 lg:w-1/4">
                             <Button
                                 className="w-full h-24 font-thin !text-6xl text-yellow-400 !bg-lime-900"
                                 action={handleIncrement}
